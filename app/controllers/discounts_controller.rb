3354ce40b328c8f9f1ecde6ec85d6cbd9bef4613
Justin Ramirez <jusrez528@gmail.com>
2022-08-07 20:54:17 -0400

    Add actions for new, create and accompanying methods

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -1,9 +1,39 @@
 class DiscountsController < ApplicationController
+  before_action :find_merchant, only: [:index, :new]
+
   def index
-    @merchant = Merchant.find(params[:merchant_id])
+   
   end
 
   def show
     
+  end
+
+  def new
+
+  end
+
+  def create
+    discount = Discount.new(discount_params)
+    if discount.save
+      redirect_to merchant_discounts_path(discount.merchant_id)
+    else
+      flash[:alert] = "Error: #{error_message(discount.errors)}"
+      redirect_to new_merchant_discount_path(discount.merchant_id)
+    end
+  end
+
+  private
+
+  def discount_params
+    params.permit(:percentage_discount, :quantity_threshold, :merchant_id)
+  end
+
+  def find_merchant
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def error_message(errors)
+   errors.full_messages.join(', ')
   end
 end
