e0768ebefae187228e6295a4ed4d38ff7a633193
Justin Ramirez <jusrez528@gmail.com>
2022-08-08 22:03:14 -0400

    Change validations to ensure discount can't be set to 100 or higher

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,5 +1,5 @@
 class Discount < ApplicationRecord
-  validates :percentage_discount, presence: true, numericality: true
+  validates :percentage_discount, presence: true, numericality: { less_than: 100 }
   validates :quantity_threshold, presence: true, numericality: true
   
   belongs_to :merchant
