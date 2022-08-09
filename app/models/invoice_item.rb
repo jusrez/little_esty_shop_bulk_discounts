21dbb28217b9d058ba5f05ec4a3b55c0e4e41dbf
Justin Ramirez <jusrez528@gmail.com>
2022-08-09 10:56:21 -0400

    Add find_best_discount method to invoice items and accompanying test

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -7,11 +7,18 @@ class InvoiceItem < ApplicationRecord
 
   belongs_to :invoice
   belongs_to :item
+  has_one :merchant, through: :item
+  has_many :discounts, through: :merchant
 
   enum status: [:pending, :packaged, :shipped]
 
   def self.incomplete_invoices
     invoice_ids = InvoiceItem.where("status = 0 OR status = 1").pluck(:invoice_id)
     Invoice.order(created_at: :asc).find(invoice_ids)
+  end
+
+  def find_best_discount
+    discounts.where('discounts.quantity_threshold <= ?', quantity)
+    .order('discounts.percentage_discount desc').first
   end
 end
