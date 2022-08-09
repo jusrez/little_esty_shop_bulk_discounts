e21d39b16e103e1a256780a516847a44ac005ab6
Justin Ramirez <jusrez528@gmail.com>
2022-08-09 13:11:34 -0400

    Complete user story merchant invoice show page: total revenue and discounted revenue

--- spec/features/invoices/show_spec.rb
+++ spec/features/invoices/show_spec.rb
@@ -51,6 +51,9 @@ RSpec.describe 'invoices show' do
     @transaction6 = Transaction.create!(credit_card_number: 879799, result: 0, invoice_id: @invoice_6.id)
     @transaction7 = Transaction.create!(credit_card_number: 203942, result: 1, invoice_id: @invoice_7.id)
     @transaction8 = Transaction.create!(credit_card_number: 203942, result: 1, invoice_id: @invoice_8.id)
+
+    @discount_a = Discount.create!(percentage_discount: 10, quantity_threshold: 5, merchant_id: @merchant1.id)
+    @discount_b = Discount.create!(percentage_discount: 15, quantity_threshold: 10, merchant_id: @merchant1.id)
   end
 
   it "shows the invoice information" do
@@ -98,6 +101,12 @@ RSpec.describe 'invoices show' do
      within("#current-invoice-status") do
        expect(page).to_not have_content("in progress")
      end
+  end
+
+  it 'shows the discounted revenue for this invoice' do
+    visit merchant_invoice_path(@merchant1, @invoice_1)
+
+    expect(page).to have_content(@invoice_1.discounted_revenue) 
   end
 
 end
